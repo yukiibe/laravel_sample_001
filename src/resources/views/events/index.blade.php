51da6366c4d5c9a63cf6330d7befcbef73d5c496
y.ibe <y.ibe@future-s.com>
2022-01-28 19:33:35 +0900

    イベント一覧寄りのCRUDに変更（骨子のみ）#53

--- src/resources/views/events/index.blade.php
+++ src/resources/views/events/index.blade.php
@@ -58,164 +58,159 @@
 
           <!-- Template For Organizer -->
           <template v-else-if="userRole == 'organizer'">
-            <v-data-table
-              :headers="headers"
-              :items="items"
-              class="elevation-1"
+            <v-btn
+              class="ma-2"
+              style="text-transform: none"
+              color="success"
+              @click="dialog = true"
             >
-              <template v-slot:top>
-                <v-dialog
-                  v-model="dialog"
-                  max-width="500px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                      class="ma-2"
-                      style="text-transform: none"
-                      color="success"
-                      v-bind="attrs"
-                      v-on="on"
-                    >
-                      New Event
-                    </v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title>
-                      <span class="text-h5">@{{ formTitle }}</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col
-                            cols="12"
-                            sm="6"
-                          >
-                            <v-text-field
-                              v-model="editedItem.title"
-                              name="title"
-                              label="Title"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col
-                            cols="12"
-                          >
-                            <v-textarea
-                              v-model="editedItem.description"
-                              name="description"
-                              label="Description"
-                              rows="4"
-                              row-height="30"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col
-                            cols="6"
-                          >
-                            <v-text-field
-                              v-model="editedItem.place"
-                              name="place"
-                              label="Place"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col
-                            cols="6"
-                          >
-                            <v-text-field
-                              v-model="editedItem.fee"
-                              name="fee"
-                              label="Fee"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col
-                            cols="12"
-                          >
-                            <v-radio-group
-                              v-model="editedItem.published"
-                              row
-                            >
-                              <v-radio
-                                label="Unpublish"
-                                name="published"
-                                :value="0"
-                              ></v-radio>
-                              <v-radio
-                                label="Publish"
-                                name="published"
-                                :value="1"
-                              ></v-radio>
-                            </v-radio-group>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="close"
-                      >
-                        Cancel
-                      </v-btn>
-                      <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="save"
-                      >
-                        Save
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-                <v-dialog
-                  v-model="dialogDelete"
-                  max-width="500px"
-                >
-                  <v-card>
-                    <v-card-title class="text-h6">Are you sure you want to delete this event?</v-card-title>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        color="green darken-1"
-                        style="text-transform: none"
-                        text
-                        @click="close"
-                      >
-                        Cancel
-                      </v-btn>
-                      <v-btn
-                        color="green darken-1"
-                        style="text-transform: none"
-                        text
-                        @click="confirmDelete"
-                      >
-                        OK
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </template>
-              <template v-slot:item.title="{ item }">
-                <td>
-                  <a href="javascript:void(0);" @click="showItem(item)">@{{ item.title }}</a>
-                </td>
-              </template>
-              <template v-slot:item.published="{ item }">
-                <td v-if="item.published">
-                  Published
-                </td>
-                <td v-else>
-                  Not Published
-                </td>
-              </template>
-              <template v-slot:item.actions="{ item }">
-                <td>
-                  <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-                  <v-icon small class="mr-2" @click="deleteItem(item)">mdi-delete</v-icon>
-                </td>
-              </template>
-            </v-data-table>
-          </template>
+              New Event
+            </v-btn>
 
+            <!-- Event Cards -->
+            <v-container>
+              <v-row>
+                <v-col
+                  v-for="item in items"
+                  :key="item.id"
+                  cols="4"
+                >
+                  <v-card
+                    class="mx-auto"
+                    max_width="344"
+                    outlined
+                    elevation="4"
+                  >
+
+                    <a href="javascript:void(0);" @click="showItem(item)">@{{ item.title }}</a>
+                    <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+                    <v-icon small class="mr-2" @click="deleteItem(item)">mdi-delete</v-icon>
+
+                  </v-card>
+                </v-col>
+              <v-row>
+            </v-container>
+
+            <!-- Form Dialog -->
+            <v-dialog
+              v-model="dialog"
+              max-width="500px"
+            >
+              <v-card>
+                <v-card-title><span class="text-h5">@{{ formTitle }}</span></v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                      >
+                        <v-text-field
+                          v-model="editedItem.title"
+                          name="title"
+                          label="Title"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                      >
+                        <v-textarea
+                          v-model="editedItem.description"
+                          name="description"
+                          label="Description"
+                          rows="4"
+                          row-height="30"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="6"
+                      >
+                        <v-text-field
+                          v-model="editedItem.place"
+                          name="place"
+                          label="Place"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="6"
+                      >
+                        <v-text-field
+                          v-model="editedItem.fee"
+                          name="fee"
+                          label="Fee"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                      >
+                        <v-radio-group
+                          v-model="editedItem.published"
+                          row
+                        >
+                          <v-radio
+                            label="Unpublish"
+                            name="published"
+                            :value="0"
+                          ></v-radio>
+                          <v-radio
+                            label="Publish"
+                            name="published"
+                            :value="1"
+                          ></v-radio>
+                        </v-radio-group>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="close"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="save"
+                  >
+                    Save
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+            <!-- Delete Dialog -->
+            <v-dialog
+              v-model="dialogDelete"
+              max-width="500px"
+            >
+              <v-card>
+                <v-card-title class="text-h6">Are you sure you want to delete this event?</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="green darken-1"
+                    style="text-transform: none"
+                    text
+                    @click="close"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    color="green darken-1"
+                    style="text-transform: none"
+                    text
+                    @click="confirmDelete"
+                  >
+                    OK
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </template>
         </v-container>
       </v-main>
 
